c967ca93aea30fa91d6d5c604307c1b8870a3d27
Curtis Ganung <curtisganung99@gmail.com>
2023-11-23 15:26:25 -0800

    Added barebones networking node

---
+++ working/curtis/network.gd
@@ -0,0 +1,109 @@
+extends Node3D
+
+# Variables
+@onready var server_ip = $ServerIP
+
+const PORT = 9999
+# const PLAYER = preload("") # This needs the URL to the character
+
+var connected_peer_ids = []
+var local_player_character
+var enet_peer = ENetMultiplayerPeer.new()
+
+var ip_array = IP.get_local_addresses()
+var ips = " ".join(ip_array)
+
+var ip_address
+
+# Unused variable for UPNP.
+var external_ip
+
+
+# Detects if text has been submitted for joining IP address,
+# assigns the correct variables, and then deals with menus.
+func _on_server_ip_text_submitted(new_text):
+	server_ip = new_text
+	$ServerIP.text = ""
+	$ServerIP.visible = false
+	$ServerIP.visible = true
+
+
+# On pressing host button, creates server, figures out joining address,
+# which is just the local IP right now, and calls function to add host
+# character
+func _on_host_pressed():
+	$NetworkInfo/NetworkSideDisplay.text = "Server Side"
+	$Menu.visible = false
+	$ServerIP.visible = false
+	
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	multiplayer.peer_connected.connect(add_player)
+	multiplayer.peer_disconnected.connect(remove_player)
+	
+	add_player(multiplayer.get_unique_id())
+	
+	print("IP addresses in array: " + ips)
+	
+	if OS.has_feature("windows"):
+		if OS.has_environment("COMPUTERNAME"):
+			ip_address = IP.resolve_hostname(str(OS.get_environment("COMPUTERNAME")), 1)
+	
+	print("IP address is most likely: " + ip_address)
+	
+	# Will remain commented out as identifier of what we tried before.
+	# upnp_setup()
+
+
+# Calls functions and deals with menus when pressing join.
+func _on_join_pressed():
+	$NetworkInfo/NetworkSideDisplay.text = "Client Side"
+	$Menu.visible = false
+	$ServerIP.visible = false
+	
+	enet_peer.create_client(server_ip, PORT)
+	multiplayer.multiplayer_peer = enet_peer
+
+
+# Function to add player characters.
+func add_player(peer_id):
+	var player = PLAYER.instantiate()
+	player.name = str(peer_id)
+	add_child(player)
+
+
+# Function to remove player characters.
+func remove_player(peer_id):
+	var player = get_node_or_null(str(peer_id))
+	if player:
+		player.queue_free()
+
+
+# Function for chat messages (probably not going to be used)
+func _on_message_input_text_submitted(new_text):
+	local_player_character.rpc("display_message", new_text)
+	$MessageInput.text = ""
+	$MessageInput.release_focus()
+
+
+# DO NOT USE: this is simply for showing what we tried,
+# this is a major computer security hazard.
+func upnp_setup():
+	var upnp = UPNP.new()
+	var discover_result = upnp.discover()
+	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, \
+	"UPNP Invalid Gateway!")
+	
+	if discover_result == UPNP.UPNP_RESULT_SUCCESS:
+		if upnp.get_gateway() and upnp.get_gateway().is_valid_gateway():
+			var map_result_udp = upnp.add_port_mapping(PORT, 0, "godot_udp", "UDP", 0)
+			var map_result_tcp = upnp.add_port_mapping(PORT, 0, "godot_tcp", "TCP", 0)
+			
+			if not map_result_udp == UPNP.UPNP_RESULT_SUCCESS:
+				upnp.add_port_mapping(PORT, 0, "", "UDP")
+			if not map_result_tcp == UPNP.UPNP_RESULT_SUCCESS:
+				upnp.add_port_mapping(PORT, 0, "", "TCP")
+	
+	external_ip = upnp.query_external_address()
+	
+	print("Success! Join address is: %s" % upnp.query_external_address())
