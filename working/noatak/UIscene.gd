77e0b640eaf936c25e6c1b8a324cd5457c47605c
Noatak Post <noatak.post@gmail.com>
2023-12-04 08:33:53 -0800

    hooked up virtual keyboard to UI

--- working/noatak/UIscene.gd
+++ working/noatak/UIscene.gd
@@ -3,14 +3,23 @@ extends VBoxContainer
 signal host_pressed()
 signal join_pressed()
 
+var textBox
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	textBox = $Header/test
+	Input.connect("key_pressed", self._on_key_pressed)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_key_pressed(event):
+	if event is InputEventKey and event.pressed:
+		var keycode = event.keycode
+		textBox.set_text(keycode)
 
 
 func _on_host_pressed():
