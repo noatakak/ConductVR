c5c20f822507d925510cbae7f881d9ccc046b4ba
NOATAK POST <noatak.post@gmail.com>
2024-04-02 19:58:36 -0700

    added new concert hall, added new player body, body rotation is currently non functional, start reimplementing IK, currently is not great,all this needs to be tested with two headsets still in order to observe behavior over network

--- characters/player character/player_character.gd
+++ characters/player character/player_character.gd
@@ -1,5 +1,18 @@
 extends XROrigin3D
 
+@export var left_hand_marker: Marker3D
+@export var right_hand_marker: Marker3D
+
+@export var camera: XRCamera3D
+
+@export var head_tracker: Node3D
+@export var left_hand_tracker: OpenXRHand
+@export var right_hand_tracker: OpenXRHand
+
+@export var char_node: Node3D
+
+var offset = Vector3(0, 0, -0.15)
+
 
 func _enter_tree():
 	set_multiplayer_authority(str(name).to_int())
@@ -15,3 +28,16 @@ func _ready():
 
 func _process(_delta):
 	if not is_multiplayer_authority(): return
+	update_body_position_and_rotation()
+	
+func update_body_position_and_rotation():
+	left_hand_marker.global_position = left_hand_tracker.global_position
+	right_hand_marker.global_position = right_hand_tracker.global_position
+	
+	var target_pos = camera.global_transform.origin
+	head_tracker.global_transform.origin = target_pos - camera.global_transform.basis.z * offset.z
+	char_node.global_transform.origin = head_tracker.global_transform.origin + Vector3(0, -1.7, 0);
+	char_node.global_rotation.y = head_tracker.global_rotation.y + PI
+
+
+
